8bb2cd9f5af3fc3d6d01422348675a5e1773d3dd
takatoshi.ichige <takatoshi.ichige>
2021-03-11 13:43:59 +0900

    Storyboardの作成、ViewControllerとの接続まで完了

---
+++ SlideshowApp/ImageDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageDetailViewController.swift
+//  SlideshowApp
+//
+//  Created by takatoshi.ichige on 2021/03/11.
+//
+
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageViiew: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // アウトレット接続　画像表示用ボタン
+    @IBOutlet weak var imageViewButton: UIButton!
+    
+    // アウトレット接続　画像表示制御ボタン類
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var autoSlideButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // ボタンタップアクション
+    // 自動再生・停止
+    @IBAction func tappedAutoSlide(_ sender: Any) {
+    }
+    
+    // 戻る
+    @IBAction func tappedBackButton(_ sender: Any) {
+    }
+    
+    // 進む
+    @IBAction func tappedNextButton(_ sender: Any) {
+    }
+    
+    
+    
 }
 
